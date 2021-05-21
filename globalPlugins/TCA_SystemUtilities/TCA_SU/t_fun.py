7f212f581d560a8d4edadcad0460832c6d19d6d7
peterrc87 <peterrc87@gmail.com>
2021-05-21 19:36:57 +0200

    Corregida función mos

---
+++ globalPlugins/TCA_SystemUtilities/TCA_SU/t_fun.py
@@ -0,0 +1,216 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#TCASystemUtilities, Shut down and restart the PC with Windows classic sound, an Open system Options.
+#Autor: Peter Reina<peterrc87@gmail.com><Tecnoconocimiento Accesible  2020>
+# This file is covered by the GNU General Public License.
+
+import os, subprocess, threading
+import ctypes 
+from threading import Thread
+import wx
+import winsound
+import ui, api, 	keyboardHandler
+
+class disable_file_system_redirection:
+
+	_disable = ctypes.windll.kernel32.Wow64DisableWow64FsRedirection
+	_revert = ctypes.windll.kernel32.Wow64RevertWow64FsRedirection
+
+	def __enter__(self):
+		self.old_value = ctypes.c_long()
+		self.success = self._disable(ctypes.byref(self.old_value))
+
+	def __exit__(self, type, value, traceback):
+		if self.success:
+			self._revert(self.old_value)
+
+def ejecutar(modo, aplicacion, parametros, directorio, ventana):
+	p = ctypes.windll.shell32.ShellExecuteW(
+		None,
+		modo,
+		aplicacion,
+		parametros,
+		directorio,
+		ventana)
+	return p
+#Clase para hilos.
+class T_h(Thread):
+	def __init__(self, frame, op):
+		super(T_h, self).__init__()
+		self.frame = frame
+		self.op = op
+		self.daemon = True
+		self.start()
+	def run(self):
+		def TCAShut():		
+			ui.message('Apagando el Pc.')
+			winsound.PlaySound('C:\Windows\Media\Windows Shutdown.wav',winsound.SND_FILENAME)
+			si = subprocess.STARTUPINFO()
+			si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+			subprocess.run('shutdown.exe -s -t 3', shell=True, startupinfo=si)
+		def TCAShutR():
+			ui.message('Reiniciando el PC.')
+			winsound.PlaySound('C:\Windows\Media\Windows Shutdown.wav',winsound.SND_FILENAME)
+			si = subprocess.STARTUPINFO()
+			si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+			subprocess.run('shutdown.exe -r -t 3', shell=True)
+	
+		def TCAShutA():
+			ui.message('anulando el apagado o reinicio del pc.')
+			subprocess.run('shutdown.exe -a', shell=True)
+
+		def TCAcopy_sys():
+			subprocess.Popen('Systeminfo | clip', shell=True)
+		def TCAList():
+			f = api.getForegroundObject()
+			try:
+				obj = f.children[1].children[2].children[0].children[0].children[0]
+			except:
+				obj = f.children[1].children[0].children[2].children[0].children[0].children[0]
+
+			c_obj=obj.name
+			a=c_obj.replace('Dirección: ', '')
+			os.chdir('{}'.format(a))
+			subprocess.Popen('dir /b|clip', shell=True)
+			ui.message('Copiada la lista al portapapeles')
+		def TCAsfc():
+			try:
+				os.environ['PROGRAMFILES(X86)']
+				with disable_file_system_redirection():
+					ejecutar('runas', 'cmd.exe', '/c' + 'sfc' + '/scannow' + '&pause', None, 10)				
+			except:
+				ejecutar('runas', 'cmd.exe', '/c' + 'sfc' + '/scannow' + '&pause', None, 10)								
+											
+			
+		def TCAcopitar():
+			lt=['powershell', 'Get-WmiObject Win32_SoundDevice |clip']
+			subprocess.run(lt, shell=True)
+			ui.message('Copiada la info del sonido al portapapeles')
+		
+		def ocu():
+			r_ac=os.getcwd()
+			f = api.getForegroundObject()
+			try:
+				obj = f.children[1].children[2].children[0].children[0].children[0]
+			except:
+				obj = f.children[1].children[0].children[2].children[0].children[0].children[0]
+			c_obj=obj.name
+			a=c_obj.replace('Dirección: ', '')
+			d=os.path.dirname(a)
+			b=os.path.basename(a)
+			os.chdir(d)
+			#keyboardHandler.KeyboardInputGesture.fromName("alt+leftarrow").send()
+
+			try:
+				os.environ['PROGRAMFILES(X86)']
+				with disable_file_system_redirection():
+
+					#subprocess.run('attrib /d /s +h {}'.format(b), shell=True)
+					ejecutar(None,'cmd.exe', '/c' + 'attrib /d /s +h {}'.format(b), None, 10)	
+				#keyboardHandler.KeyboardInputGesture.fromName("alt+leftarrow").send()
+				
+				keyboardHandler.KeyboardInputGesture.fromName("alt+f4").send()
+				os.startfile(d)
+				winsound.Beep(900,300)
+				os.chdir(r_ac)
+			except:
+				ejecutar(None,'cmd.exe', '/c' + 'attrib /d /s +h'.format(b), None, 10)		
+				#keyboardHandler.KeyboardInputGesture.fromName("alt+leftarrow").send()
+				
+				keyboardHandler.KeyboardInputGesture.fromName("alt+f4").send()
+				os.startfile(d)
+				#subprocess.run('attrib -h {}'.format(b), shell=True)
+				winsound.Beep(900,300)
+				os.chdir(r_ac)
+
+		
+		def mos():
+			r_ac = os.getcwd()
+			f = api.getForegroundObject()
+			try:
+				obj = f.children[1].children[2].children[0].children[0].children[0]
+			except:
+				obj = f.children[1].children[0].children[2].children[0].children[0].children[0]
+			c_obj=obj.name
+			a=c_obj.replace('Dirección: ', '')
+			#d=os.path.dirname(a)
+			#b=os.path.basename(a)
+			os.chdir(a)
+			try:
+				os.environ['PROGRAMFILES(X86)']
+				with disable_file_system_redirection():  
+					subprocess.Popen('attrib /d -h', shell=True)
+					#ejecutar(None,'cmd.exe','/c' + 'attrib /d /s -h', None, 10)		
+					os.chdir(r_ac)
+					winsound.Beep(300,300)
+			except:
+				#ejecutar('runas','cmd.exe','/c' + 'attrib /d -h', None, 10)		
+				subprocess.Popen("attrib /d -h", shell = True)
+				os.chdir(r_ac)
+				winsound.Beep(300,300)
+
+
+		
+		def clean():
+			if os.path.isfile(os.path.join(os.environ["userprofile"], "appdata", "Roaming", "nvda", "tsu.ini"),  ):
+				pass
+			else:
+				dlg=wx.RichMessageDialog(None,"Si es la primera vez que ejecuta ésta acción.\n es necesario crear un perfil de limpieza, solo debe hacerlo una vez.\n puede pulsar en Crear perfil también si desea mmofificar uno existente, o puede marcar  la casilla para no volver a mostrar éste mensaje.", style=wx.CANCEL) 
+				dlg.SetOKLabel("Crear perfil")
+				dlg.ShowCheckBox("No volver  a mostrar este mensaje")		
+				rp = dlg.ShowModal()
+				if dlg.IsCheckBoxChecked():
+					with open(os.path.join(os.environ["userprofile"], "appdata", "Roaming", "nvda", "tsu.ini"), "w") as tsu_i:
+						tsu_i.write("sageset: True")
+				else:
+					pass
+				
+				if rp == wx.ID_OK:
+					try:
+						os.environ['PROGRAMFILES(X86)']
+						with disable_file_system_redirection(): 
+							#try:
+							ejecutar('runas','cmd.exe','/c' + 'CLEANMGR /sageset:1', None, 10)	
+					
+					except:
+						ejecutar('runas','cmd.exe', '/c' + 'CLEANMGR /sageset:1', None, 10)		
+				else:
+					dlg.Destroy()
+			
+			try:
+				os.environ['PROGRAMFILES(X86)']
+				with disable_file_system_redirection():  
+					ejecutar('runas','cmd.exe', '/c' + 'CLEANMGR /sagerun:1', None, 10)	
+					
+			except:
+				ejecutar('runas','cmd.exe', '/c' + 'CLEANMGR /sagerun:1', None, 10)				
+				
+
+
+
+
+			
+
+		
+
+		if self.op == 4:
+			wx.CallAfter(TCAcopy_sys)
+		elif self.op == 1:
+			wx.CallAfter(TCAShut)
+		elif self.op == 2:
+			wx.CallAfter(TCAShutR)
+		elif self.op == 3:
+			wx.CallAfter(TCAShutA)
+		elif self.op == 5:
+			wx.CallAfter(TCAList)
+		elif self.op == 6:
+			wx.CallAfter(TCAsfc)
+		elif self.op == 7:
+			wx.CallAfter(TCAcopitar)
+		elif self.op == 8:
+			wx.CallAfter(ocu)
+		elif self.op == 9:
+			wx.CallAfter(mos)
+		elif self.op == 10:
+			wx.CallAfter(clean)
+
---
+++ globalPlugins/TCA_SystemUtilities/__init__.py
@@ -0,0 +1,149 @@
+﻿# -*- coding: UTF-8 -*-
+#TCASystemUtilities, Shut down and restart the PC with Windows classic sound, an Open system Options.
+#Autor: Peter Reina<peterrc87@gmail.com><Tecnoconocimiento Accesible  2020>
+# This file is covered by the GNU General Public License.
+from scriptHandler import script
+import api
+import keyboardHandler
+import globalPluginHandler
+import subprocess, os, sys 
+import webbrowser
+import tones
+import ui
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+from TCA_SU import t_fun as tsu
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):	
+	@script(description='Apagar el sistema', category='TCA-SystemUtilities', gesture='kb:nvda+shift+a')
+	def script_TCAShut(self,gesture):		
+		tsu.T_h(self,1)
+	
+	@script(description='Reinicio del sistema', category='TCA-SystemUtilities', gesture='kb:nvda+shift+r')
+	def script_TCAShutR(self,gesture):
+		
+		tsu.T_h(self, 2)
+	
+	@script(description='Anular apagado', category='TCA-SystemUtilities', gesture='kb:nvda+0')
+	def script_TCAShutA(self,gesture):
+		tsu.T_h(self, 3)
+		
+	@script(description='Abrir Explorador Windows', category='TCA-SystemUtilities', gesture='kb:nvda+e')
+	def script_OpenEx(self,gesture):
+		os.system('explorer')
+	
+	@script(description='Abrir opciones de sonido', category='TCA-SystemUtilities', gesture='kb:nvda+shift+3')
+	def script_TCARexplo(self,gesture):
+		subprocess.Popen('mmsys.cpl', shell=True)
+	
+	@script(description='Abrir carpeta Roaming', category='TCA-SystemUtilities', gesture='kb:nvda+9')
+	def script_TCARoa(self,gesture):
+		os.startfile(os.path.join(os.environ['userprofile'], 'appdata', 'Roaming'))
+	
+	@script(description='Abrir Asistente transferir archivos por Bluetooth  ', category='TCA-SystemUtilities', gesture='kb:nvda+shift+9')
+	def script_TCAopti(self,gesture):
+		subprocess.Popen('fsquirt')
+			 
+	@script(description='Saber la arquitectura del sistema', category='TCA-SystemUtilities')
+	def script_TCAar(self,gesture):
+		try:
+			os.environ['PROGRAMFILES(X86)']
+			version_sys=64
+		except:
+			version_sys=32
+		ui.message('Su sistema operativo es de: {} Bits'.format(version_sys))
+		
+	@script(description='Abrir el asistente para guardar la contraseña del sistema', category='TCA-SystemUtilities')
+	def script_TCArcon(self,gesture):
+		ui.message('Abriendo restablecer   contraseñas de Windows')
+		subprocess.Popen('RunDll32.exe keymgr.dll,PRShowSaveWizardExW')
+	
+	@script(description='Copiar al portapapeles la ruta', category='TCA-SystemUtilities')
+	def script_TCAclip(self,gesture):
+		f = api.getForegroundObject()
+		try:
+			obj = f.children[1].children[2].children[0].children[0].children[0]
+		except:
+			obj = f.children[1].children[0].children[2].children[0].children[0].children[0]
+		c_obj=obj.name
+		a=c_obj.replace('Dirección: ', '')
+		api.copyToClip('"{}"'.format(a))
+		ui.message('Ruta copiada ')
+		
+	@script(description='Abrir el administrador de discos', category='TCA-SystemUtilities')
+	def script_TCAdisk(self,gesture):
+		subprocess.Popen('diskmgmt.msc', shell=True)
+	
+	@script(description='Abrir la tienda oficial de complementos', category='TCA-SystemUtilities', gesture='kb:nvda+x')
+	def script_TCAtien(self,gesture):
+		webbrowser.open_new_tab('https://addons.nvda-project.org/index.es.html')
+	
+	@script(description='Abrir monitor de recursos', category='TCA-SystemUtilities')
+	def script_TCAmon(self, gesture):
+		subprocess.Popen('resmon')
+	
+	@script(description='Copiar al portapapeles la lista de carpetas y archivos', category='TCA-SystemUtilities', gesture='kb:nvda+shift+l')
+	def script_TCAList(self, gesture):
+		tsu.T_h(self, 5)
+	
+	@script(description='Saber la versión de Windows', category='TCA-SystemUtilities')
+	def script_TCAver(self, gesture):
+		subprocess.Popen('winver')
+	
+	@script(description='Abrir el administrador de dispositivoss', category='TCA-SystemUtilities')
+	def script_TCAdispo(self,gesture):
+		subprocess.Popen('devmgmt.msc', shell=True)
+	
+	@script(description='Abrir Opciones de carpeta', category='TCA-SystemUtilities', gesture='kb:nvda+shift+0')
+	def script_TCAcar(self,gesture):
+		subprocess.Popen('control folders')
+		tones.beep(350,100)
+	
+	@script(description='Hacer análisis del sistema Con SFC  ', category='TCA-SystemUtilities', gesture='kb:nvda+shift+4')
+	def script_TCAsfc(self, gesture):
+		tsu.T_h(self, 6)
+			
+	@script(description='Copiar información de todo el sistema al portapapeles', category='TCA-SystemUtilities', gesture='kb:nvda+shift+6')
+	def script_TCAcopy_sys(self, gesture):
+		tsu.T_h(self, 4)
+		ui.message('información del sistema copiada al portapapeles')
+	
+	@script(description='Abrir mapa de caracteres', category='TCA-SystemUtilities', gesture='kb:nvda+shift+7')
+	def script_TCAchar(self, gesture):
+		subprocess.Popen('charmap')
+	
+	@script(description='Abrir Asistente guardar contraseñas de usuarios', category='TCA-SystemUtilities', gesture='kb:nvda+shift+8')
+	def script_TCAa_usu(self, gesture):
+		subprocess.Popen('credwiz')
+
+	@script(description='Abrir Optimizar las unidades', category='TCA-SystemUtilities', gesture='kb:nvda+shift+1')
+	def script_TCAoptim(self, gesture):
+		try:
+			os.environ['PROGRAMFILES(X86)']
+			with tsu.disable_file_system_redirection():
+				subprocess.Popen('dfrgui')	
+		except:
+			subprocess.Popen('dfrgui')
+	
+	@script(description='Abrir opciones de voz', category='TCA-SystemUtilities', gesture='kb:nvda+shift+2')
+	def script_TCAopvox(self, gesture):
+		subprocess.Popen(os.path.join(os.environ['systemroot'], 'system32', 'Speech', 'SpeechUX', 'sapi.cpl'), shell=True)
+	
+	@script(description='Copiar al portapapeles la información sobre las tarjetas de sonido', category='TCA-SystemUtilities', gesture='kb:nvda+shift+5')
+	def script_TCAcopitar(self, gesture):
+		tsu.T_h(self, 7)
+		
+	
+	@script(description='Ocultar carpetas', category='TCA-SystemUtilities', gesture='kb:nvda+shift+f4')
+	def script_TCAocu(self, gesture):
+		tsu.T_h(self, 8)
+		
+		
+	
+	@script(description='Mostrar carpetas ocultas', category='TCA-SystemUtilities', gesture='kb:nvda+shift+f5')
+	def script_TCAmos(self, gesture):
+		tsu.T_h(self, 9)
+	
+	@script(description='Limpiar disco', category='TCA-SystemUtilities', gesture='kb:nvda+shift+f6')
+	def script_TCAclean(self, gesture):
+		tsu.T_h(self, 10)
+		
